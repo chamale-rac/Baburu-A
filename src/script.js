007e0467798d5c0a8c6a06d9a71aec793e55eb5e
Denis <mccass02@gmail.com>
2025-01-26 11:24:20 -0600

    MainMenu Sonido arreglado
    GG

--- src/script.js
+++ src/script.js
@@ -2,3 +2,7 @@ document.getElementById('playButton').addEventListener('click', function() {
     var audio = new Audio("/assets/audio/jump.mp3"); 
     audio.play();
 });
+
+document.getElementById('closeButton').addEventListener('click', function() {
+    window.close();
+});
